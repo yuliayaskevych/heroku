724c49aa85d9ffb64d362ed7a823382abcbc21f9
Yuliia Yaskevych <yulia.yaskevych@gmail.com>
2021-08-27 18:12:59 +0300

    add server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/steam-clone'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/steam-clone/index.html'))
+});
+
+app.listen(process.env.PORT || 8080);
